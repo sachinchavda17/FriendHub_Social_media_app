944e40d5bac1b6c1f8f768a3b9cde1a5203c3040
sachinchavda17 <sachinchavda321@gmail.com>
2024-02-14 22:32:00 +0530

    streak

--- client/src/scenes/loginPage/Login.jsx
+++ client/src/scenes/loginPage/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
-import Form from "./Form"
+import Form from "./Form";
 const LoginPage = () => {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -17,16 +17,16 @@ const LoginPage = () => {
         </Typography>
       </Box>
       <Box
-      width={isNonMobileScreens?"50%":"93%"}
-      p="2rem"
-      m="2rem auto"
-      borderRadius={"1.5rem "}
-      backgroundColor={theme.palette.background.alt}
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius={"1.5rem "}
+        backgroundColor={theme.palette.background.alt}
       >
-<Typography fontWeight={"bold"} variant="h5" sx={{mb:"1.5rem "}} >
-  Welcome to FriendsHub, the Social Media for Sociopaths!
-</Typography>
-<Form/>
+        <Typography fontWeight={"bold"} variant="h5" sx={{ mb: "1.5rem " }}>
+          Welcome to FriendsHub, the Social Media for Sociopaths!
+        </Typography>
+        <Form />
       </Box>
     </Box>
   );
